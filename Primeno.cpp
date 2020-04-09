4fc767956d6c587852d41d4353f33ed9e60a7029
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-04-09 22:04:19 +0530

    Update Primeno.cpp

--- Primeno.cpp
+++ Primeno.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+using namespace std;
+int i=2,no;
+int PRIME(int n)
+{
+	if(i==n)
+		return 1;
+	else if(n%i==0)
+		return 0;
+	else
+	{
+		i+=1;
+		return PRIME;
+	}
+}
+int main()
+{
+	cout<<"Enter the number\t";
+	cin>>no;
+	if(PRIME(no)==1)
+		cout<<"It is a prime number.";
+	else
+		cout<<"It is not a prime number.";
+}
