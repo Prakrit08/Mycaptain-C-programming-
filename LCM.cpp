4cf029908f244cb6e805db0eff060cfd92486c24
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-04-09 22:17:48 +0530

    Update LCM.cpp

--- LCM.cpp
+++ LCM.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+int LCM(int a, int b)
+{ 
+    static int c=1;
+ 	if (c%a==0 && c%b==0)
+        return c;
+    c++;
+    LCM(a, b);
+    return c;
+}
+int main()
+{
+	int a,b;
+	cout<<"Enter first number\t";
+	cin>>a;
+	cout<<"Enter Second number\t";
+	cin>>b;
+	cout<<"The LCM of "<<a<<" and "<<b<<" is "<<LCM(a,b);
+}
