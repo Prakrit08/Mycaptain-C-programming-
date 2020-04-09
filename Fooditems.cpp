c803033481055f44377d62de7a5b51d96447fb74
Prakrit08 <63188483+Prakrit08@users.noreply.github.com>
2020-04-09 19:58:04 +0530

    Update Fooditems.cpp

--- Fooditems.cpp
+++ Fooditems.cpp
@@ -4,4 +4,33 @@ int main()
 {
     do
     {
-        
+        int c,p,i=0;
+        string fi;
+        cin>>c;
+        switch(c)
+            case 1:
+                fi="Pizza"
+                p=239,i=1;
+                break;
+            case 2:
+                fi="Pasta"
+                p=179,i=1;
+                break;
+            case 3:
+                fi="Burger"
+                p=129,i=1;
+                break;
+            case 4:
+                fi="Sandwich"
+                p=149,i=1;
+                break;
+            case 5:
+                fi="French Fries"
+                p=99,i=1;
+                break;
+            default:
+                cout<<"Please Enter A Valid Choice 1-5\n";
+        if(i==1)
+            cout<<"Food item - "<<fi<<"\nPrice - Rs "<<p;
+    }while(!(c>0 && c<6));
+                
