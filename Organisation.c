9c074788f3dec1b3635f697bb5bcbf4e554227df
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-04-25 18:30:36 +0530

    Update Organisation.c

--- Organisation.c
+++ Organisation.c
@@ -4,12 +4,12 @@ struct Emp
 {
 	int empno,sal;
 	char empnm[50],depnm[50];
-}
+};
 void disp(Emp e[])
 {
 	for(int i=0;i<n;i++)
 		printf("%d\t%s\t%s\t%d\n",e[i].empno,e[i].empnm,e[i].depnm,e[i].sal);
-}
+};
 int main()
 {
 	Emp e[n];
