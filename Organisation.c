7b94ffd7e6b2f8dad586561dc2eedbf130af977b
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-04-25 19:14:11 +0530

    Update Organisation.c

--- Organisation.c
+++ Organisation.c
@@ -4,16 +4,16 @@ struct Emp
 {
 	int empno,sal;
 	char empnm[50],depnm[50];
-};
-void disp(Emp e[])
+}e[n];
+void disp(int i)
 {
-	for(int i=0;i<n;i++)
-		printf("%d\t%s\t%s\t%d\n",e[i].empno,e[i].empnm,e[i].depnm,e[i].sal);
+	printf("%d\t%s\t%s\t%d\n",e[i].empno,e[i].empnm,e[i].depnm,e[i].sal);
 };
 int main()
 {
-	Emp e[n];
-	for(int i=0;i<n;i++)
-		scanf("%d %s %s %d ",e[i].empno,e[i].empnm,e[i].depnm,e[i].sal);
-	disp(e);
+	int i;
+	for(i=0;i<n;i++)
+		scanf("%d %s %s %d",&e[i].empno,&e[i].empnm,&e[i].depnm,&e[i].sal);
+	for(i=0;i<n;i++)
+		disp(i);
 }
