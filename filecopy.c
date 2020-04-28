af69b164808e39b1b965a471b178fabdb2c60c68
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-04-28 22:40:13 +0530

    Update filecopy.c

--- filecopy.c
+++ filecopy.c
@@ -1,1 +1,22 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main() 
+{ 
+	FILE *fptr1,*fptr2; 
+	char f1[100],f2[100],c; 
+	printf("Enter the filename to open for reading \n"); 
+	scanf("%s",f1); 
+	fptr1=fopen(f1,"r"); 
+	printf("Enter the filename to open for writing \n"); 
+	scanf("%s",f2); 
+	fptr2=fopen(f2,"w"); 
+	c=fgetc(fptr1); 
+	while (c!=EOF) 
+	{ 
+		fputc(c,fptr2); 
+		c=fgetc(fptr1); 
+	} 
+`	fclose(fptr1); 
+	fclose(fptr2); 
+}
 
