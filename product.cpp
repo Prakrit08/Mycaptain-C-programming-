b01d75ae424f1aa43046477543fbcce101902986
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-05-02 19:47:24 +0530

    Update product.cpp

--- product.cpp
+++ product.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
 using namespace std;
-template <class T>
-class pro
-{
-	T a,b;
-	public:
-		void prod(T a,T b)
-		{
-			cout<<"Product = "<<a*b;
-		}
-};
 int main()
 {
-	pro p;
-	cin>>p.a>>p.b;
-	prod(p.a,p.b);
+	float a,b;
+	cin>>a>>b;
+	cout<<"Product = "<<a*b;	
 }
