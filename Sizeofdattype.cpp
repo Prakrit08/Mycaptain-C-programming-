130b7b16ac542110f3bab11e42baa4f4eb9d41e2
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-05-02 19:31:03 +0530

    Update Sizeofdattype.cpp

--- Sizeofdattype.cpp
+++ Sizeofdattype.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	cout<<"Size of char: "<<sizeof(char)<<" byte\n";
-	cout<<"Size of int: "<<sizeof(int)<<"bytes\n";
-	cout<<"Size of float: "<<sizeof(float)<<"bytes\n";
-	cout<<"Size of double: "<<sizeof(double)<<"bytes\n";
+	cout<<"Size of int: "<<sizeof(int)<<" bytes\n";
+	cout<<"Size of float: "<<sizeof(float)<<" bytes\n";
+	cout<<"Size of double: "<<sizeof(double)<<" bytes\n";
 }
